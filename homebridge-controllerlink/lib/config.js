3474f24e4778b2172d7b03f73fda053c239f9faf
Kraig McConaghy <kraigm@enferra.com>
2016-01-09 13:01:06 -0600

    Refactored to prepare for expansion

--- homebridge-controllerlink/lib/config.js
+++ homebridge-controllerlink/lib/config.js
@@ -5,26 +5,34 @@
 var Loader = require('./loader.js');
 var fs = require('fs');
 
-var getConfig = function(homebridge) {
+// Look for the configuration file
+var getConfigPath = function(homebridge) {
 	var User = homebridge ? homebridge.user : Loader.User;
+	return User && User.configPath();
+};
 
-	// Look for the configuration file
-	var configPath = User.configPath();
-
-	// Complain and exit if it doesn't exist yet
+// Complain and exit if it doesn't exist yet
+var ensureConfigFile = function(configPath) {
 	if (!fs.existsSync(configPath)) {
 		throw new Error("Couldn't find a config.json file at '" + configPath + "'. Look at config-sample.json for examples of how to format your config.js and add your home accessories.");
 	}
+};
 
-	// Load up the configuration file
-	var config;
+// Load up the configuration file
+var readConfigFile = function(configPath) {
+	ensureConfigFile(configPath);
+
 	try {
-		config = JSON.parse(fs.readFileSync(configPath));
+		return JSON.parse(fs.readFileSync(configPath));
 	}
 	catch (err) {
 		throw new Error("There was a problem reading your config.json file.\nPlease try pasting your config.json file here to validate it: http://jsonlint.com\n" + err);
 	}
+};
 
+var getConfig = function(homebridge) {
+	var configPath = getConfigPath(homebridge);
+	var config = readConfigFile(configPath);
 	return {
 		Config: config
 	};
