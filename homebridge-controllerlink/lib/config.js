f3ddcc89fc98ccc7753b1314a3f9152563d49fe6
Kraig McConaghy <kraigm@enferra.com>
2016-01-09 11:02:50 -0600

    Restructured config.js to define methods outside export

--- homebridge-controllerlink/lib/config.js
+++ homebridge-controllerlink/lib/config.js
@@ -5,29 +5,31 @@
 var Loader = require('./loader.js');
 var fs = require('fs');
 
-module.exports = {
-	get: function(homebridge) {
-		var User = homebridge ? homebridge.user : Loader.User;
+var getConfig = function(homebridge) {
+	var User = homebridge ? homebridge.user : Loader.User;
 
-		// Look for the configuration file
-		var configPath = User.configPath();
+	// Look for the configuration file
+	var configPath = User.configPath();
 
-		// Complain and exit if it doesn't exist yet
-		if (!fs.existsSync(configPath)) {
-			throw new Error("Couldn't find a config.json file at '" + configPath + "'. Look at config-sample.json for examples of how to format your config.js and add your home accessories.");
-		}
-
-		// Load up the configuration file
-		var config;
-		try {
-			config = JSON.parse(fs.readFileSync(configPath));
-		}
-		catch (err) {
-			throw new Error("There was a problem reading your config.json file.\nPlease try pasting your config.json file here to validate it: http://jsonlint.com\n" + err);
-		}
-
-		return {
-			Config: config
-		};
+	// Complain and exit if it doesn't exist yet
+	if (!fs.existsSync(configPath)) {
+		throw new Error("Couldn't find a config.json file at '" + configPath + "'. Look at config-sample.json for examples of how to format your config.js and add your home accessories.");
 	}
+
+	// Load up the configuration file
+	var config;
+	try {
+		config = JSON.parse(fs.readFileSync(configPath));
+	}
+	catch (err) {
+		throw new Error("There was a problem reading your config.json file.\nPlease try pasting your config.json file here to validate it: http://jsonlint.com\n" + err);
+	}
+
+	return {
+		Config: config
+	};
+};
+
+module.exports = {
+	get: getConfig,
 };
