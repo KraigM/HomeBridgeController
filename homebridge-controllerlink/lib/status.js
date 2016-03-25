e220a25d6df4fad21cd5218e0a96ee1f97a9c316
Kraig McConaghy <kraigm@enferra.com>
2016-03-25 15:52:58 -0500

    [#31] Changed restart process to async style

--- homebridge-controllerlink/lib/status.js
+++ homebridge-controllerlink/lib/status.js
@@ -3,26 +3,33 @@
  */
 
 const ChildProcess = require('child_process');
+const Promise = require('bluebird');
 
 var restartHomeBridge = function(log) {
 	log = log || console.log;
-	debug = log.debug || log;
+	var debugLog = log.debug ? log.debug.bind(log) : log;
+	var errorLog = log.error ? log.error.bind(log) : log;
 
-	try {
-		log('restarting...');
+	log('restarting...');
 
-		var args = process.argv.slice();
-		var cmd = args.shift();
-		log(JSON.stringify(args));
-		ChildProcess.spawn(cmd, args, {
-			detached: true,
-			stdio: 'ignore'
+	return Promise.resolve()
+		.then(function(){
+			log('starting next hub...');
+			var args = process.argv.slice();
+			var cmd = args.shift();
+			debugLog(JSON.stringify(args));
+			ChildProcess.spawn(cmd, args, {
+				detached: true,
+				stdio: 'ignore'
+			});
+		})
+		.catch(function(err){
+			errorLog('Error starting next homebridge hub. \n'+(err ? err.stack || err.message || err : err));
+		})
+		.finally(function(){
+			log('waiting for exit...');
+			process.exit(0);
 		});
-	} catch (err) {
-		var errorLog = log.error || log;
-		if (errorLog) errorLog('Error restarting homebridge. \n'+(err ? err.stack || err.message || err : err));
-	}
-	process.exit(0);
 };
 
 module.exports = {
